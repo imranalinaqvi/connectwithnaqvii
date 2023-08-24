24115f56d917fda60c8db0bbe710aeb736e066eb
Syed Imran <syedimranmurtaza32@gmail.com>
2023-08-24 13:00:38 -0700

    error fixing

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+)
+
